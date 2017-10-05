77feace9e27cb98339cb2b281cf4a8639d2de440
Samuel <samueloluwatommzy@gmail.com>
2017-10-06 00:10:42 +0100

    Database connection established using mongodb

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ let express = require('express');
 let bodyParser = require('body-parser')
 let morgan = require('morgan');
 let mongoose = require('mongoose')
+let methodOverride = require('method-override');
+let db = require('./config/database');
 
-let port = 8000;
+
+mongoose.connect(db.database,function(err){
+    if(err){
+        console.log("connection to database failed")
+    }
+    console.log("Connected to database")
+})
+
+let port = 8080;
 var app = express();
 mongoose.connect()
  
-app.listen(port, function(err){
-    if(err){
-        console.log("could not connect to port")
+app.listen(port, function(){
+    
+        console.log("app listening on port " + port)
     }
-    else{
-        console.log("app listening on " + port )
-    }
-});
+);
---
+++ config/database.js
@@ -0,0 +1,7 @@
+"use strict"
+
+module.exports = {
+    database: "mongodb://127.0.0.1:27017/student_resource",
+    database_Name: "Andela_project",
+    secret_key: "aledna"
+}
