f3ceda1b28fdf71ba3ba8e07d6221f0638873706
Samuel <samueloluwatommzy@gmail.com>
2017-10-17 00:38:40 +0100

    some new dependencies required

--- app.js
+++ app.js
@@ -4,27 +4,38 @@ let morgan = require('morgan');
 let mongoose = require('mongoose')
 let methodOverride = require('method-override');
 let db = require('./config/database');
+let http = require("http").Server(app);
+let socket_io = require('socket.io')(http);
 let  cors = require('cors');
+
 
 
 mongoose.connect(db.database,function(err){
     if(err){
         console.log("connection to database failed")
     }
-    console.log("Connected to database")
-})
+    else{
+    console.log("Connected to database" +  db.database);
+}});
 
 let port = 8080;
 var app = express();
 mongoose.connect()
  
-app.listen(port, function(){
-    
+http.listen(port, function(err){
+    if(err){
+        console.log(err)
+    }
+    else{
         console.log("app listening on port " + port)
     }
+}
 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use("/",(req,res) =>{
-    res.sendFile(__dirname + "/index.html")
-})
+app.use(morgan("dev"));
+var api = require("./routes/api")(app, express,socket_io);
+app.use("/api", api);
+// app.use("/",(req,res) =>{
+//     res.sendFile(__dirname + "/index.html")
+// })
