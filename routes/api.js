de36bd04c3a950ddc2699dc9bcc30f4c31d69870
Samuel <samueloluwatommzy@gmail.com>
2017-10-25 22:40:12 +0100

    Create,Read,Update and delete

--- routes/api.js
+++ routes/api.js
@@ -12,21 +12,19 @@ function createToken(user){
 
 module.exports  = function(app, express,socket_io){
     let api = express.Router();
-  // CREATE  Student Endpoint
-      api.post("/api/createStudent", (request, response) => {
-        const studentObj = {
+  // CREATE  Student Endpoint 
+    api.post("/api/create", (request, response) => {
+            const studentObj = {
             firstName: request.body.firstName,
+            lastName: request.body.lastName,
             email: request.body.email,
             userName: request.body.userName,
             password: request.body.password,
             lastName: request.body.lastName,
-            department: request.body.department,
-            level: request.body.level,
-            courses: request.body.courses
+            department: request.body.department
         }
-        console.log(studentObj);
-        const student = new Student(studentObj)
-        student.save((err, savedUser) => {
+
+        Student.find({ firstName: studentObj.firstName, lastName : studentObj.lastName, available: true }, (err, students) => {
             if (err) {
                 response.status(200).send({
                     status: 403,
@@ -36,19 +34,41 @@ module.exports  = function(app, express,socket_io){
                 })
                 return
             }
-            response.status(200).send({
-                status: 200,
-                success: true,
-                message: "Student added successfully",
-                student_data: savedUser
-            })
 
+            if (students && students.length > 0) {
+                response.status(200).send({
+                    status: 200,
+                    success: false,
+                    message: "This name already exists, please try again"
+                })
+            } else {
+                const student = new Student(studentObj)
+
+                student.save((err, savedStudent) => {
+                    if (err) {
+                        response.status(200).send({
+                            status: 403,
+                            success: false,
+                            message: "Error occured",
+                            error_message: err.message
+                        })
+                        return
+                    }
+
+                    response.status(200).send({
+                        status: 200,
+                        success: true,
+                        message: "Student added successfully",
+                        student_data: savedStudent
+                    })
+                })
+            }
         })
     })
 
     //authentication or login endpoint
 
-    api.post("/login", (request, response) => {
+    api.post("api/login", (request, response) => {
           const userObj = {
             userName: request.body.username,
             password: request.body.password,
@@ -145,8 +165,127 @@ module.exports  = function(app, express,socket_io){
                                         token: student_token,
                                         data: student
                                     })
-                                })}})}}})})
-                                                                                                                                                                                     
+                                })
+                            }}
+                            )}
+                        }})
+                    })
+
+          /* Update Student Endpoint   */
+        api.put("/api/update", (request, response) => {
+        const _student_id = request.body._student_id
+
+        const studentObj = {
+            firstName: request.body.firstName,
+            lastName: request.bode.lastName
+        }
+
+        Student.findOne({ _id: _student_id, available: true }, (err, student) => {
+            if (err) {
+                response.status(200).send({
+                    status: 403,
+                    success: false,
+                    message: "Error occured",
+                    error_message: err.message
+                })
+                return
+            }
+
+            if (!student) {
+                response.status(200).send({
+                    status: 200,
+                    success: false,
+                    message: "Trying to edit a non-existing student"
+                })
+            } else {
+                student.firstName = studentObj.fullname
+                student.lastName = studentObj.lastName
+
+                student.save((err, savedStudent) => {
+                    if (err) {
+                        response.status(200).send({
+                            status: 403,
+                            success: false,
+                            message: "Error occured",
+                            error_message: err.message
+                        })
+                        return
+                    }
+
+                    response.status(200).send({
+                        status: 200,
+                        success: true,
+                        message: "Student updated successfully",
+                        student_data: savedStudent
+                    })
+
+                })
+            }
+
+        })
+
+    })
+
+    /*Delete Student Endpoint*/
+    api.delete("/api/delete", (request, response) => {
+        const _student_id = request.body.student_id
+
+        Student.findOne({ _id: _student_id, available: true }, (err, student) => {
+            if (err) {
+                response.status(200).send({
+                    status: 403,
+                    success: false,
+                    message: "Error occured",
+                    error_message: err.message
+                })
+                return
+            }
+
+            if (!student) {
+                response.status(200).send({
+                    status: 200,
+                    success: false,
+                    message: "Trying to delete a non-existing student"
+                })
+            } else {
+                student.available = false
+
+                student.save((err, savedStudent) => {
+                    if (err) {
+                        response.status(200).send({
+                            status: 403,
+                            success: false,
+                            message: "Error occured",
+                            error_message: err.message
+                        })
+                        return
+                    }
+
+                    User.update({ _id: savedStudent._user_id }, { $set: { available: false } }, (err) => {
+                        if (err) {
+                            response.status(200).send({
+                                status: 403,
+                                success: false,
+                                message: "Error occured",
+                                error_message: err.message
+                            })
+                            return
+                        }
+
+                        response.status(200).send({
+                            status: 200,
+                            success: true,
+                            message: "Student deleted successfully"
+                        })
+
+                    })
+
+                })
+            }
+
+        })
+    })
+                                                                                                                                                    
 
     return api;
              
